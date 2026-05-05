cce79374bb9142820fb35953c2c51c5bf5b2df76
Gian Ferraz <93200583+ReversaOfficial@users.noreply.github.com>
2026-05-05 14:55:48 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const fs     = require('fs');
 const path   = require('path');
 const crypto = require('crypto');
 
-const PORT    = 3000;
+const PORT    = process.env.PORT || 3000;
 const DB_FILE = path.join(__dirname, 'db.json');
 const SECRET  = 'arathorn_secret_key_2024_change_in_production';
 
